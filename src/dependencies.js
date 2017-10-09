168472f817b1ce8b86a0e766dd6274c07224e3f4
Balashev Konstantin <balashev.konstantin@nicetu.spb.ru>
2017-10-09 15:03:28 +0300

    qwebchannel added globally

--- src/dependencies.js
+++ src/dependencies.js
@@ -1,4 +1,4 @@
-require('./QWebChannel');
+window.QWebChannel = require('./QWebChannel');
 THREE = require('../node_modules/three/build/three');
 require('../node_modules/three/examples/js/controls/EditorControls');
 require('../node_modules/three/examples/js/controls/TransformControls');
